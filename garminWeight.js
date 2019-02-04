b848477d3635cb17af547e2e282b012c3212d685
Jannick Garthen <jannick.garthen@gmail.com>
2019-02-04 23:22:14 +0100

    Replace phantomjs with puppeteer

--- garminWeight.js
+++ garminWeight.js
@@ -1,54 +1,72 @@
-import phantom from 'phantom';
-import credentials from './credentials.json';
+import puppeteer from "puppeteer";
+import credentials from "./credentials.json";
 
-const loginAddress = 'https://sso.garmin.com/sso/login?service=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&webhost=olaxpw-conctmodern010&source=https%3A%2F%2Fconnect.garmin.com%2Fde-DE%2Fsignin&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=de_DE&id=gauth-widget&cssUrl=https%3A%2F%2Fstatic.garmincdn.com%2Fcom.garmin.connect%2Fui%2Fcss%2Fgauth-custom-v1.2-min.css&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&usernameShown=false&displayNameShown=true&consumeServiceTicket=false&initialFocus=true&embedWidget=false&generateExtraServiceTicket=false&globalOptInShown=false&globalOptInChecked=false';
-const successAddress = 'https://connect.garmin.com/modern/';
+const loginAddress =
+  "https://sso.garmin.com/sso/login?service=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&webhost=olaxpw-conctmodern010&source=https%3A%2F%2Fconnect.garmin.com%2Fde-DE%2Fsignin&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=de_DE&id=gauth-widget&cssUrl=https%3A%2F%2Fstatic.garmincdn.com%2Fcom.garmin.connect%2Fui%2Fcss%2Fgauth-custom-v1.2-min.css&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&usernameShown=false&displayNameShown=true&consumeServiceTicket=false&initialFocus=true&embedWidget=false&generateExtraServiceTicket=false&globalOptInShown=false&globalOptInChecked=false";
+const successAddress = "https://connect.garmin.com/modern/";
 
-const waitForEvaluated = (page) => (fn, checker) => new Promise((resolve) => {
-  const id = setInterval(async () => {
-    let res = await page.evaluate(fn);
-    if (checker(res)) {
-      clearInterval(id);
-      resolve(res);
-    }
-  }, 1000);
-});
+const waitForEvaluated = page => (fn, checker) =>
+  new Promise(resolve => {
+    const id = setInterval(async () => {
+      try {
+        let res = await page.evaluate(fn);
+        if (checker(res)) {
+          clearInterval(id);
+          resolve(res);
+        }
+      } catch (e) {}
+    }, 1000);
+  });
 
 export default async () => {
-  const instance = await phantom.create();
-  const page = await instance.createPage();
-  const status = await page.open(loginAddress);
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+  const page = await browser.newPage();
+  await page.goto(loginAddress, { waitUntil: "networkidle2" });
 
-  if (status !== 'success') {
-    instance.exit();
-    throw new Error('FAIL to load the address');
-  }
+  await page.evaluate(
+    (username, password) => {
+      document.getElementById("username").value = username;
+      document.getElementById("password").value = password;
+      document.getElementById("login-form").submit();
+    },
+    credentials.garmin_username,
+    credentials.garmin_password
+  );
 
-  await page.evaluate((username, password) => {
-    document.getElementById('username').value = username;
-    document.getElementById('password').value = password;
-    document.getElementById('login-form').submit();
-  }, credentials.garmin_username, credentials.garmin_password);
-
-  await waitForEvaluated(page)(() => location.href, (res) => res === successAddress);
-  await page.evaluate((from, to) => location.href = 'https://connect.garmin.com/modern/proxy/userprofile-service/userprofile/personal-information/weightWithOutbound/filterByDay?from=' + from + '&until=' + to, Date.now() - (1000 * 60 * 60 * 24), Date.now());
+  await waitForEvaluated(page)(
+    () => location.href,
+    res => res === successAddress
+  );
+  await page.evaluate(
+    (from, to) =>
+      (location.href =
+        "https://connect.garmin.com/modern/proxy/userprofile-service/userprofile/personal-information/weightWithOutbound/filterByDay?from=" +
+        from +
+        "&until=" +
+        to),
+    Date.now() - 1000 * 60 * 60 * 24,
+    Date.now()
+  );
   const weight = await waitForEvaluated(page)(() => {
     try {
       const dataset = JSON.parse(document.body.children[0].innerHTML)[0];
       return {
         value: dataset.weight / 1000,
-        unit: 'kg',
-        date: dataset.date,
+        unit: "kg",
+        date: dataset.date
       };
-    } catch(e) {
+    } catch (e) {
       return false;
     }
   }, Boolean);
 
-  instance.exit(1);
+  await browser.close();
+
   return {
     value: weight.value,
     unit: weight.unit,
-    date: new Date(weight.date),
+    date: new Date(weight.date)
   };
 };
--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-import google from 'googleapis';
-import promisify from 'es6-promisify';
+import google from "googleapis";
+import { promisify } from "es6-promisify";
 
-import oauth2Client from './oauth2Client';
-import requestFitAuth from './authGoogle';
-import getRecentWeight from './garminWeight';
-import storage from './storage';
-import credentials from './credentials.json';
+import oauth2Client from "./oauth2Client";
+import requestFitAuth from "./authGoogle";
+import getRecentWeight from "./garminWeight";
+import storage from "./storage";
+import credentials from "./credentials.json";
 
-// monkey patch console log for logs with date 
+// monkey patch console log for logs with date
 const originalLog = console.log;
-global.console.log = (...args) => originalLog(`[${new Date().toISOString()}]`, ...args);
+global.console.log = (...args) =>
+  originalLog(`[${new Date().toISOString()}]`, ...args);
 
 const OAuth2 = google.auth.OAuth2;
-const fitness = google.fitness('v1');
+const fitness = google.fitness("v1");
 const dataSourceCreate = promisify(fitness.users.dataSources.create);
-const dataSourceDatasetsPatch = promisify(fitness.users.dataSources.datasets.patch);
+const dataSourceDatasetsPatch = promisify(
+  fitness.users.dataSources.datasets.patch
+);
 const dataSourceDatasetsGet = promisify(fitness.users.dataSources.datasets.get);
 
 google.options({ auth: oauth2Client });
 
 (async () => {
-  console.log('sync started...');
-  let tokens = await storage.get('tokens');
+  console.log("sync started...");
+  let tokens = await storage.get("tokens");
   if (!tokens) {
     requestFitAuth();
-    console.log('Accept access and copy the code query parameter from the redirect url into the terminal and hit enter');
+    console.log(
+      "Accept access and copy the code query parameter from the redirect url into the terminal and hit enter"
+    );
 
-    const splitter = require('split')();
+    const splitter = require("split")();
     const stdInListener = process.stdin.pipe(splitter);
     tokens = await new Promise((resolve, reject) => {
-      stdInListener.on('data', function setCredentials(code) {
+      stdInListener.on("data", function setCredentials(code) {
         console.log(`receive code \`${code}\``);
-        stdInListener.removeListener('data', setCredentials);
+        stdInListener.removeListener("data", setCredentials);
         process.stdin.unpipe(splitter);
 
         oauth2Client.getToken(code, async (err, tokens) => {
@@ -43,76 +48,84 @@ google.options({ auth: oauth2Client });
         });
       });
     });
-    await storage.set('tokens', tokens);
+    await storage.set("tokens", tokens);
   }
 
   oauth2Client.setCredentials(tokens);
 
-  let dataStreamId = await storage.get('dataStreamId');
+  let dataStreamId = await storage.get("dataStreamId");
   // if data stream was not stored, create it
   if (!dataStreamId) {
     const dataSource = await dataSourceCreate({
       resource: dataSourceResource,
-      userId: 'me',
+      userId: "me"
     });
-    await storage.set('dataStreamId', dataSource.dataStreamId);
+    await storage.set("dataStreamId", dataSource.dataStreamId);
     dataStreamId = dataSource.dataStreamId;
   }
 
   const weight = await getRecentWeight();
   const dataSourceBody = createDataset(dataStreamId, weight);
-  const datasetId = `${dataSourceBody.minStartTimeNs}-${dataSourceBody.maxEndTimeNs}`;
+  const datasetId = `${dataSourceBody.minStartTimeNs}-${
+    dataSourceBody.maxEndTimeNs
+  }`;
   const remoteDataSource = await dataSourceDatasetsGet({
     dataSourceId: dataStreamId,
     datasetId,
     limit: 1,
-    userId: 'me',
+    userId: "me"
   });
-  
+
   if (remoteDataSource.point.length >= 1) {
-    console.log(`Duplicated entry found: ${weight.date}, ${weight.value} ${weight.unit}`);
+    console.log(
+      `Duplicated entry found: ${weight.date}, ${weight.value} ${weight.unit}`
+    );
   } else {
     await dataSourceDatasetsPatch({
       dataSourceId: dataStreamId,
       datasetId,
       resource: dataSourceBody,
-      userId: 'me',
+      userId: "me"
     });
-    console.log(`Successfully created dataset: ${weight.date}, ${weight.value} ${weight.unit}`);
+    console.log(
+      `Successfully created dataset: ${weight.date}, ${weight.value} ${
+        weight.unit
+      }`
+    );
   }
-  console.log('sync ended!');
+  console.log("sync ended!");
 })();
 
 const dataSourceResource = {
-  application: { name: 'Garmin Googlefit' },
+  application: { name: "Garmin Googlefit" },
   dataType: {
-    field: [
-      { name: 'weight', format: 'floatPoint' },
-    ],
-    name: 'com.google.weight',
+    field: [{ name: "weight", format: "floatPoint" }],
+    name: "com.google.weight"
   },
   device: {
-    manufacturer: 'Garmin',
-    model: 'Index Smart Scale',
-    type: 'scale',
+    manufacturer: "Garmin",
+    model: "Index Smart Scale",
+    type: "scale",
     uid: credentials.garmin_uid,
-    version: '3.0.0.0',
+    version: "3.0.0.0"
   },
-  type: 'raw',
+  type: "raw"
 };
 
 const createDataset = (dataStreamId, weight) => {
   const start = weight.date.getTime() * 1000 * 1000;
-  const end = (weight.date.getTime() * 1000 * 1000) + 110;
+  const end = weight.date.getTime() * 1000 * 1000 + 110;
   return {
     dataSourceId: dataStreamId,
     minStartTimeNs: start,
     maxEndTimeNs: end,
-    point: [{
-      dataTypeName: 'com.google.weight',
-      startTimeNanos: start,
-      endTimeNanos: end,
-      value: [{ fpVal: weight.value }],
-    }],
+    point: [
+      {
+        dataTypeName: "com.google.weight",
+        startTimeNanos: start,
+        endTimeNanos: end,
+        value: [{ fpVal: weight.value }]
+      }
+    ]
   };
 };
